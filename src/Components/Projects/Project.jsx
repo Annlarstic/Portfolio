da075f1ece492e3f76259538339f7205edd05338
Annlarstic <nonsochi1823@gmail.com>
2025-10-23 02:49:41 +0100

    PORTFOLIO

--- src/Components/Projects/Project.jsx
+++ src/Components/Projects/Project.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
+// import me6 from '/Images/me6.jpeg';
+import me04 from '/Images/me04.jpg'
 import me3 from '/Images/me3.jpeg';
-import me4 from '/Images/me4.jpeg';
 import me5 from '/Images/me5.jpeg';
 import './Project.css'
 
@@ -13,13 +14,15 @@ export default function Project() {
     <div className='container project_container'>
 
     <article className='project_item'>
-    <div className='project_item_image'>
-    <img src={me3} alt="" />
-    <h3>Weather</h3>
+    <div className='project_item_image1'>
+    <img src={me04} alt="" />
+    <h3>MovieBox</h3>
     <small className='text-light'>React</small>
+    
+    
 
     <div className='project_item_btn'>
-     <a href='https://weather-forecast-ten-kohl.vercel.app/' className='btn'>Vercel</a>
+     <a href='https://box-movie-abqk3hvdf-ann-nonsos-projects.vercel.app' className='btn'>Vercel</a>
    
     </div>
     </div>
@@ -28,11 +31,11 @@ export default function Project() {
 
     <article className='project_item'>
     <div className='project_item_image'>
-    <img src={me4} alt="" />
-    <h3>BMI</h3>
-    <small className='text-light'>JavaScript</small>
+    <img src={me3} alt="" />
+    <h3>Wheather</h3>
+    <small className='text-light'>React</small>
     <div className='project_item_btn'>
-   <a href='https://bmi-body-weight-316o4tzyj-ann-nonsos-projects.vercel.app/' className='btn'>Vercel</a>
+   <a href='https://weather-forecast-ten-kohl.vercel.app/' className='btn'>Vercel</a>
    
 
     </div>
